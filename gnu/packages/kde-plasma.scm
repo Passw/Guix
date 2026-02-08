de7001d8347076c2df9cde455690de4b526573bf
Sughosha <sughosha@disroot.org>
2026-02-08 23:05:33 +0530

    gnu: ksystemstats: Update to 6.5.5.
    * gnu/packages/kde-plasma.scm (ksystemstats): Update to 6.5.5.
    
    Change-Id: I2566455e67063ff94bf5247403f4b65528cde869

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -1200,7 +1200,7 @@ call it if it is not associated to a terminal.")
 (define-public ksystemstats
   (package
     (name "ksystemstats")
-    (version "6.5.2")
+    (version "6.5.5")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/"
@@ -1208,7 +1208,7 @@ call it if it is not associated to a terminal.")
                                   version ".tar.xz"))
               (sha256
                (base32
-                "1jkgi3js84lwakb853i2qgmk0nrk0arm2c4ygf9x1r9g4jip5hnq"))))
+                "17ffml9280f75p73m5b0mji5h2q577sfck2kv3y24kq87m09f465"))))
     (build-system qt-build-system)
     (arguments
      (list #:qtbase qtbase
