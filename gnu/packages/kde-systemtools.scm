fda436b15b682aa79a0355e70a6428893c5d4473
Sughosha <sughosha@disroot.org>
2026-02-08 23:59:27 +0530

    gnu: krfb: Update to 25.12.2.
    * gnu/packages/kde-systemtools.scm (krfb): Update to 25.12.2.
    
    Change-Id: Ia9ee621bb65d37c88f998c0e46b3cc5897a312cd

--- gnu/packages/kde-systemtools.scm
+++ gnu/packages/kde-systemtools.scm
@@ -407,14 +407,14 @@ This package is part of the KDE base applications module.")
 (define-public krfb
   (package
     (name "krfb")
-    (version "25.12.0")
+    (version "25.12.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/krfb-" version ".tar.xz"))
        (sha256
-        (base32 "139d958g5yxw901b7ylba1fir9fyw4821y4g8mmjq7j59gshlsh1"))))
+        (base32 "09bh051s0h0c3fg7nljh1yp8l7hilq6ncp62w4qmyr3zcqrnvj5w"))))
     (build-system qt-build-system)
     (arguments (list #:qtbase qtbase
                      #:tests? #f
