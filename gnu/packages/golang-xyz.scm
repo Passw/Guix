61ff8d4cc443ee9aa8a784a7aceb0c66fbad8cb1
Arun Isaac <arunisaac@systemreboot.net>
2026-02-08 23:23:20 +0000

    gnu: Add go-github-com-blevesearch-bleve.
    * gnu/packages/golang-xyz.scm (go-github-com-blevesearch-bleve): New variable.
    
    Change-Id: I07aabae62d1a508d92138d4f1574a93edfd13b1b
    Modified-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -3114,6 +3114,64 @@ based on murmurhash.")
               (delete-file-recursively
                (string-append "src/" import-path "/examples")))))))))
 
+(define-public go-github-com-blevesearch-bleve
+  (package
+    (name "go-github-com-blevesearch-bleve")
+    (version "2.5.7")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/blevesearch/bleve")
+              (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0vszzxj76236phd1iq5r31jccnxv8i20ykxgmffmfqy3g215njh0"))
+       (snippet
+        #~(begin
+            (use-modules (guix build utils))
+            (delete-file-recursively "cmd/bleve/vendor")))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/blevesearch/bleve"
+      #:test-flags
+      #~(list "-vet=off")))
+    (native-inputs
+     (list go-github-com-blevesearch-go-metrics
+           go-github-com-blevesearch-goleveldb
+           go-github-com-blevesearch-stempel
+           go-github-com-blevesearch-snowball
+           go-github-com-spf13-cobra))
+    (propagated-inputs
+     (list go-github-com-bits-and-blooms-bitset
+           go-github-com-blevesearch-bleve-index-api
+           go-github-com-blevesearch-geo
+           go-github-com-blevesearch-go-porterstemmer
+           go-github-com-blevesearch-gtreap
+           go-github-com-blevesearch-scorch-segment-api
+           go-github-com-blevesearch-segment
+           go-github-com-blevesearch-snowballstem
+           go-github-com-blevesearch-upsidedown-store-api
+           go-github-com-blevesearch-vellum
+           go-github-com-blevesearch-zapx-v11
+           go-github-com-blevesearch-zapx-v12
+           go-github-com-blevesearch-zapx-v13
+           go-github-com-blevesearch-zapx-v14
+           go-github-com-blevesearch-zapx-v15
+           go-github-com-blevesearch-zapx-v16
+           go-github-com-couchbase-moss
+           go-github-com-golang-protobuf
+           go-github-com-roaringbitmap-roaring
+           go-go-etcd-io-bbolt
+           go-golang-org-x-text))
+    (home-page "https://github.com/blevesearch/bleve")
+    (synopsis "Go indexing and search library")
+    (description
+     "Bleve is a Go library for indexing and searching text, numeric,
+geo-spatial and vector data.")
+    (license license:asl2.0)))
+
 (define-public go-github-com-blevesearch-bleve-index-api
   (package
     (name "go-github-com-blevesearch-bleve-index-api")
