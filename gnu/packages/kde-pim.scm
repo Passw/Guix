566776369c3afb05ea67edf27aee996e414804eb
Sughosha <sughosha@disroot.org>
2026-02-08 23:59:13 +0530

    gnu: akonadi-contacts: Update to 25.12.2.
    * gnu/packages/kde-pim.scm (akonadi-contacts): Update to 25.12.2.
    
    Change-Id: Ief1c9646059092988a2693bdb8d7dc0f49839734

--- gnu/packages/kde-pim.scm
+++ gnu/packages/kde-pim.scm
@@ -334,14 +334,14 @@ collection and item views.")
 (define-public akonadi-contacts
   (package
     (name "akonadi-contacts")
-    (version "25.12.0")
+    (version "25.12.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/akonadi-contacts-" version ".tar.xz"))
        (sha256
-        (base32 "0xala89nypbf3jgi5mkpxsywrfa1b5zkd70xmnfnz2n3xs382ljz"))))
+        (base32 "11inxc12lk4hbc26g1axy8k080qgxqrqrilp93agfy1l2jannkgs"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules))
