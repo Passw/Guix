45f2ae1c007c9bc6c4d5f6279939399fad949b00
Sughosha <sughosha@disroot.org>
2026-02-08 23:58:48 +0530

    gnu: kqtquickcharts: Update to 25.12.2.
    * gnu/packages/kde-education.scm (kqtquickcharts): Update to 25.12.2.
    
    Change-Id: Ib8e35f807fe20541e07336c160f6d4ec277d40d0

--- gnu/packages/kde-education.scm
+++ gnu/packages/kde-education.scm
@@ -65,14 +65,14 @@
 (define-public kqtquickcharts
   (package
     (name "kqtquickcharts")
-    (version "25.12.0")
+    (version "25.12.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/release-service/"
                                   version "/src/kqtquickcharts-" version ".tar.xz"))
               (sha256
                (base32
-                "0lv8ikda7ny6ayns55khlc0hwgbiz6sms7h3qs66pr5sy828p2xn"))))
+                "0z2421ffvxfp85pl80203g3pd3xg8dh60rd6zxy182bkr5l5hymw"))))
     (build-system qt-build-system)
     (native-inputs (list extra-cmake-modules))
     (inputs (list qtdeclarative))
