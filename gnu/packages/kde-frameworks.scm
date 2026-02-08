4cf95bee7e542eaa997a33490ac17559339bdc77
Sughosha <sughosha@disroot.org>
2026-02-09 01:58:03 +0530

    gnu: kwidgetsaddons: Update to 6.22.0.
    * gnu/packages/kde-frameworks.scm (kwidgetsaddons): Update to 6.22.0.
    
    Change-Id: I0dc57185bf7bf597e8af78eb18ff90681a347ee7

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1631,7 +1631,7 @@ represented by a QPoint or a QSize.")
 (define-public kwidgetsaddons
   (package
     (name "kwidgetsaddons")
-    (version "6.21.0")
+    (version "6.22.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1640,7 +1640,7 @@ represented by a QPoint or a QSize.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1g6ji535lcx3wlzfzgvdjfdkgfg0ns4jkjsp3815xhrid113ia8l"))))
+                "0jvmxh5d3jkns66zvc3cgsdmfrnpp1qvm6c23qjd6kh5jz32m0z8"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules qttools))
